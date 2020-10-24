0d9dd2376311660fad0a24807d0e4ebed98bbcdd
Adam Wolyn <adamwolyn@berkeley.edu>
2020-10-24 23:12:07 +0000

    part 2

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -8,8 +8,8 @@ class MoviesController < ApplicationController
 
   def index
     @all_ratings = Movie.all_ratings
-    ratings = params['ratings']
-    @ratings_to_show = ratings.keys
+    print(params['ratings'])
+    @ratings_to_show = params['ratings'].keys
     @movies = Movie.with_ratings(ratings.keys)
   end
 
